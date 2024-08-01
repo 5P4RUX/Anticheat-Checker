3a2a5575354ada74f45d1c9c6ce13ea9152be669
SPARUX666 <147306534+Sparux-666@users.noreply.github.com>
2024-08-01 08:44:28 +0300

    ANTI-CHEAT CHECKER.lua

---
+++ ANTI-CHEAT CHECKER.lua
@@ -0,0 +1,10 @@
+gg.clearResults()--Github: SPARUX-666
+gg.setRanges(4)
+gg.searchNumber("67109633", 4)
+if gg.getResultCount() >= 8000 then
+gg.alert("Anticheat Is High\n\nGithub: SPARUX-666")
+else
+gg.alert("Now Anticheat Is Low\n\nGithub: SPARUX-666")
+gg.clearResults()
+end
+
