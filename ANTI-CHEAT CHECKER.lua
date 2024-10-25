882f34f642e497675225d582e5f3d5413a2233ca
SP4RUX <147306534+5P4RUX@users.noreply.github.com>
2024-10-25 14:48:22 +0300

    Update ANTI-CHEAT CHECKER.lua

--- ANTI-CHEAT CHECKER.lua
+++ ANTI-CHEAT CHECKER.lua
@@ -1,10 +1,10 @@
-gg.clearResults()--Github: SPARUX-666
+gg.clearResults()--Github: 5P4RUX
 gg.setRanges(4)
 gg.searchNumber("67109633", 4)
 if gg.getResultCount() >= 8000 then
-gg.alert("Anticheat Is High\n\nGithub: SPARUX-666")
+gg.alert("Anticheat Is High\n\nGithub: 5P4RUX")
 else
-gg.alert("Now Anticheat Is Low\n\nGithub: SPARUX-666")
+gg.alert("Now Anticheat Is Low\n\nGithub: 5P4RUX")
 gg.clearResults()
 end
 
